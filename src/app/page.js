4a53035c64d188eb0cc72600e53bc4f323de25a3
sharif <srka780@gmail.com>
2024-09-21 14:09:22 +0600

    new branch sharif

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import HomePage from "../components/HomePage/HomePage";
 export default function Home() {
   return (
     <main>
+      {/* now connect with sharif */}
       <HomePage></HomePage>
     </main>
   );
