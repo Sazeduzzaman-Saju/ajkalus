bacb6ce4e093899160de5ccbf5ee188a8fc5149b
sharif <srka780@gmail.com>
2024-09-21 17:21:21 +0600

    heandle the not found page with 404 page route by dynamic route control

--- src/app/[...dynamicCategory]/not-found.js
+++ src/app/[...dynamicCategory]/not-found.js
@@ -4,7 +4,7 @@ export default function NotFound() {
   return <div>
       <h1>Not found {"-"} 404!</h1>
       <div>
-        <Link href="/">Go back to Home</Link>
+        <Link href="/">Go back to Home sharif code dev</Link>
       </div>
   </div>
 }
